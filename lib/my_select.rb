b24d0e6e7454714e3e7a347bcab151d534f41661
Sarah Anderson <saranderson13@gmail.com>
2019-03-20 20:59:39 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,10 @@
 def my_select(collection)
   i = 0
+  test_elements = []
   selected_elements = []
   while i < collection.length
-    yield collection[i]
+    test_elements << yield(collection[i])
+    if test_elements[i]
     i += 1
   end
   selected_elements
